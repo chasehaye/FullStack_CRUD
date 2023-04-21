0e568d2aac4d83d237c4d11bee510ce720ea4320
chasehaye <chasehayejoc@gmail.com>
2023-04-21 13:38:33 -0700

    intital commit

---
+++ server.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport')
+const methodOverride = require('method-override');
